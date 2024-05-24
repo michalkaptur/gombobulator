1c33bcac8f16ab56679373a29749afacf450cdba
Michal Kaptur <kaptur.michal@gmail.com>
2024-05-24 12:14:06 +0200

    init http server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "I'm the mighty calculator, the result is 42\n")
+}
 
 func main() {
-	fmt.Println("I'm the mighty calculator, the result is 42")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":7890", nil)
 }
