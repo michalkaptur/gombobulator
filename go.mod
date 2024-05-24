66b7e4c2425e9371c11fb5b99635574bca9b8435
Michal Kaptur <kaptur.michal@gmail.com>
2024-05-24 12:36:52 +0200

    added logging to all the requests

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module gombobulator
 
 go 1.22.0
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -13,8 +15,20 @@ func add(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "I'm the mighty calculator, but I don't know how to calculate sum just yet\n")
 }
 
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logrus.WithFields(logrus.Fields{"remote_addr": r.RemoteAddr, "method": r.Method, "url": r.URL}).Debug("Got a request")
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	logrus.SetLevel(logrus.DebugLevel) //TODO configure
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/add", add)
-	http.ListenAndServe(":7890", nil)
+	logrus.Info("starting the server")
+	err := http.ListenAndServe(":7890", logRequest(http.DefaultServeMux))
+	if err != nil {
+		logrus.WithError(err).Error("Failed to start the server")
+	}
 }
