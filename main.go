d96fc60f35006db362b74ce51567c0f9da26c297
Michal Kaptur <kaptur.michal@gmail.com>
2024-05-24 12:17:03 +0200

    added new handler: add

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "I'm the mighty calculator, the result is 42\n")
 }
 
+func add(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "I'm the mighty calculator, but I don't know how to calculate sum just yet\n")
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/add", add)
 	http.ListenAndServe(":7890", nil)
 }
