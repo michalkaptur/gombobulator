6f6382cba0240901e471374e8cc795fc896f4d0d
Michal Kaptur <kaptur.michal@gmail.com>
2024-05-27 12:06:01 +0200

    table tests, supposedly best practice

--- main_test.go
+++ main_test.go
@@ -4,11 +4,25 @@ import (
 	"testing"
 )
 
+var addition_tests = []struct {
+	in  []int
+	out int
+}{
+	{[]int{2, 2}, 4},
+	{[]int{1}, 1},
+	{[]int{1, 1, 1, 1, 1, 1}, 6},
+	{[]int{}, 0},
+	{[]int{0}, 0},
+	{[]int{10000000, 1}, 10000001},
+}
+
 func TestAddition(t *testing.T) {
-	data := []int{2, 2}
-	got := add(data)
-	want := 4
-	if got != want {
-		t.Errorf("got %v but wanted %v", got, want)
+	for _, pair := range addition_tests {
+		got := add(pair.in)
+		want := pair.out
+		if got != want {
+			t.Errorf("got %v but wanted %v", got, want)
+		}
+
 	}
 }
